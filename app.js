717f20357a3774c3b7b8a8f6e576befae1888d8e
JaneshkaF <janeshkaf@aol.com>
2022-11-19 02:20:11 +0000

    done

---
+++ app.js
@@ -0,0 +1,32 @@
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar){
+    let valueInYen = valueInDollar * 127.9;
+    return valueInYen
+}
+
+const fromYenToPound = function(valueInYen){
+    let valueInEuro = valueInYen * 0.8;
+    return valueInEuro
+}
+
+const sum = (a,b) => {
+    return a + b
+}
+
+console.log(sum(7,3))
+
+
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen,  fromYenToPound }
